1fc56f11d87116620048a58e02c03911ab50e5e0
v1nh1shungry <1792795667@qq.com>
2022-05-26 11:14:03 +0800

    fix const methods

--- sakura/elaina/ast.h
+++ sakura/elaina/ast.h
@@ -12,37 +12,43 @@ namespace elaina {
 
 struct Ast {
   enum Type { INTEGER, STRING, IDENTIFIER, EXPRESSION, COMMAND };
+
   virtual ~Ast() = default;
-  virtual Type type() = 0;
+  virtual Type type() const = 0;
 };
 
 struct IntegerAst : public Ast {
   explicit IntegerAst(const std::string &value) : value(std::stoi(value)) {}
-  Type type() override { return INTEGER; }
+  Type type() const override { return INTEGER; }
+
   int value;
 };
 
 struct StringAst : public Ast {
   explicit StringAst(const std::string &value) : value(value) {}
-  Type type() override { return STRING; }
+  Type type() const override { return STRING; }
+
   std::string value;
 };
 
 struct IdentifierAst : public Ast {
   explicit IdentifierAst(const Token &value) : identifier(value) {}
-  Type type() override { return IDENTIFIER; }
+  Type type() const override { return IDENTIFIER; }
+
   Token identifier;
 };
 
 struct ExpressionAst : public Ast {
-  Type type() override { return EXPRESSION; }
+  Type type() const override { return EXPRESSION; }
+
   Token op;
   std::unique_ptr<Ast> lhs;
   std::unique_ptr<Ast> rhs;
 };
 
 struct CommandAst : public Ast {
-  Type type() override { return COMMAND; }
+  Type type() const override { return COMMAND; }
+
   Token command;
   std::vector<std::unique_ptr<Ast>> args;
 };
--- sakura/elaina/object.h
+++ sakura/elaina/object.h
@@ -10,22 +10,24 @@ namespace elaina {
 struct Object {
   enum Type { INTEGER, STRING };
   virtual ~Object() = default;
-  virtual Type type() = 0;
-  virtual int getInt() { return {}; }
-  virtual std::string getString() { return {}; }
+  virtual Type type() const = 0;
+  virtual int getInt() const { return {}; }
+  virtual std::string getString() const { return {}; }
 };
 
 struct Integer : public Object {
   explicit Integer(int value) : value(value) {}
-  Type type() override { return INTEGER; }
-  int getInt() override { return value; }
+  Type type() const override { return INTEGER; }
+  int getInt() const override { return value; }
+
   int value;
 };
 
 struct String : public Object {
   explicit String(const std::string &value) : value(value) {}
-  Type type() override { return STRING; }
-  std::string getString() override { return value; }
+  Type type() const override { return STRING; }
+  std::string getString() const override { return value; }
+
   std::string value;
 };
 
