e34165a112e91fc8161bea74859197f396ad12bf
v1nh1shungry <1792795667@qq.com>
2022-05-28 22:05:55 +0800

    static link in mingw

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ add_requires("fmt", "magic_enum", "nlohmann_json", "sfml")
 set_languages("c++20")
 
 if is_plat("mingw") and is_mode("release") then
-    add_ldflags("-mwindows")
+    add_ldflags("-mwindows", "-static")
 end
 
 target("elaina")
